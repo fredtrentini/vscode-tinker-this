6c76dcd26942ec8d23682427250e6cc7af7a2552
Fred <fredtrentini@gmail.com>
2024-07-07 19:17:12 -0300

    Remove single line comments before running

--- src/extension.ts
+++ src/extension.ts
@@ -48,14 +48,54 @@ export function activate(context: vscode.ExtensionContext) {
 				selection === undefined || selection.anchor.isEqual(selection.active) ?
 					editor.document.getText() : editor.document.getText(selection);
 
-			return text
-				.replace(/<\?php/, '')
-				.replace(/\r?\n/g, '')
-				.replace(/\ +/g, ' ')
-				.replace(/"+/g, '\\"')
-				.replace(/\$+/g, "\\$");
+			return prepareCode(text);
 		}
 	}
+
+    function prepareCode(text: string) {
+        const textWithoutComments = removeComments(text);
+    
+        return textWithoutComments
+        .replace(/<\?php/, '')
+        .replace(/\r?\n/g, '')
+        .replace(/\ +/g, ' ')
+        .replace(/"+/g, '\\"')
+        .replace(/\$+/g, "\\$");
+    }
+    
+    function removeComments(text: string) {
+        const commentInStringRanges = getMatchRanges(text, /(['"])(.|\n)*?(?<!\\)\1/g);
+        const commentRanges = getMatchRanges(text, /\/\/.*/g).reverse();
+    
+        for (const commentRange of commentRanges) {
+            if (isRangeCollidingWithRanges(commentRange, commentInStringRanges)) {
+                continue;
+            }
+    
+            text = text.slice(0, commentRange[0]) + text.slice(commentRange[1]);
+        }
+        
+        return text;
+    }
+    
+    function getMatchRanges(text: string, regex: RegExp) {
+        const ranges: Array<Array<number>> = [];
+    
+        text.replace(regex, (...args) => {
+            const match = args[0];
+            const offset = args[args.length - 2];
+
+            ranges.push([offset, offset + match.length]);
+        });
+    
+        return ranges;
+    }
+    
+    function isRangeCollidingWithRanges(targetRange: Array<number>, ranges: Array<Array<number>>) {
+        return ranges.some((range: Array<number>) => {
+            return !(targetRange[1] <= range[0] || targetRange[0] >= targetRange[1]);
+        });
+    }
 
 	context.subscriptions.push(...disposables);
 }
